a50525743df3c412c34533b6439440517dd09556
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-04 19:00:33 +0300

    Add path to dotenv.config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-dotenv.config();
-
+dotenv.config({ path: "../.env" });
+console.log(process.env.MONGO_URI);
 import express from "express";
 import cors from "cors";
 
