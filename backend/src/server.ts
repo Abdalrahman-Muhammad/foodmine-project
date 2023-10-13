ec162095ee03ec4e390ec9f33bdae9fe554fd990
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-13 21:28:47 +0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 
 import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
+import orderRouter from "./routers/order.router";
 import { dbConnect } from "./configs/database.config";
 
 dbConnect();
@@ -23,6 +24,7 @@ app.use(
 //routes
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
 const port = 5000;
 app.listen(port, () => {
