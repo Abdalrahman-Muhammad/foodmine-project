0bf06341ecd2703b68f200196cced7ab7eb36529
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-13 21:25:23 +0300

    Add: food page component

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
+import { UserService } from 'src/app/services/user.service';
 import { Food } from 'src/app/shared/models/Food';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-food-page',
@@ -12,11 +14,14 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class FoodPageComponent implements OnInit {
   food!: Food;
+  user!: User;
+  id!: any;
   constructor(
     private activatedRoute: ActivatedRoute,
     private foodService: FoodService,
     private cartService: CartService,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) {}
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -25,10 +30,30 @@ export class FoodPageComponent implements OnInit {
           this.food = food;
         });
     });
+
+    this.userService.userObservable.subscribe((user) => {
+      this.user = user;
+    });
+
+    this.activatedRoute.params.subscribe((params) => {
+      this.id = params['id'];
+    });
   }
 
   addToCart() {
     this.cartService.addToCart(this.food);
     this.router.navigateByUrl('/cart-page');
+  }
+
+  deleteFood() {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['id'])
+        this.foodService.deleteFood(params['id']).subscribe({
+          next: () => this.router.navigateByUrl('/'),
+        });
+    });
+  }
+  editFood() {
+    this.router.navigateByUrl(`food/${this.id}/edit`);
   }
 }
